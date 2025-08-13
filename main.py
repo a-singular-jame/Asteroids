fc615fa5922888b09c1cab0c3addab1aa1865309
a-singular-jame <james.m.farrell@sky.com>
2025-08-13 19:43:01 +0100

    implemented player ship

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    Clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))    
+    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,9 +17,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt = Clock.tick(60) / 1000
+
+        dt = clock.tick(60) / 1000
+        
+
+
         
 
 
