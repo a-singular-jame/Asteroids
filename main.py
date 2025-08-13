884ee62140d0676f9993082be7f468431a29ec7b
a-singular-jame <james.m.farrell@sky.com>
2025-08-13 22:00:18 +0100

    created groups to reduce clutter later on

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))    
+     
     dt = 0
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))   
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        Player.update(player, dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
