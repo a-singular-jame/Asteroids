724c6c0f418eca69cdec25ff9f6cbab39b2fe779
a-singular-jame <james.m.farrell@sky.com>
2025-08-18 16:56:25 +0100

    added moving asteroids and the field they spawn in

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import *
+from asteroidfield import *
 
 def main():
+    # ------- Initialization ------- #
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # ------------------------------ #
     
-     
+    # ----------- Groups ----------- #
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    # ------------------------------- #
+
+    # ---------- Variables ---------- #
+
     dt = 0
-    
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
+
+    # ------------------------------- #
+
+    # -------- Start-up text -------- #
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
+    # ------------------------------- #
 
-    Player.containers = (updatable, drawable)
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))   
-    
+    # ----- Running code begins ----- #
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,11 +54,8 @@ def main():
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-        
-
-
-        
-
+    
+    # ------------------------------- #
 
 if __name__ == "__main__":
     main()
